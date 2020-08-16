c25aae8a7bc86083dbb8dffd6bcb182bfc3d947b
Vasudev Purohit <vasudev.purohit@gmail.com>
2020-08-16 15:31:32 +0530

    First commit

---
+++ advanced_lane_finding.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 12:57:20 2020
+
+@author: vasud
+"""
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+#calculating the distortion coeffecients and the camera matrix
+
+#undistorting a real world image
+
+#thresholding based on color and gradient
+
+#perspective transformation
+
+#finding polynomials for lane lines
